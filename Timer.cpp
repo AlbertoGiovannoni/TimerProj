c4b1c3e6231cdbaec975dc6daf8e07272caf928d
alberto <alberto.giovannoni@stud.unifi.it>
2021-04-06 17:41:46 +0200

    Implementazione getter duration

--- Timer.cpp
+++ Timer.cpp
@@ -3,6 +3,33 @@
 //
 
 #include "Timer.h"
+#include <cmath>
 
+using namespace std;
+using namespace std::chrono;
 
+Timer::Timer() {
+    start = steady_clock::now();
+    duration = ::duration<int>::zero();
+    running = false;
 
+}
+
+const time_point<steady_clock> &Timer::getStart() const{
+    return start;
+}
+
+int Timer::getDuration() {
+    if(!running) {
+        return (int)round(duration.count()/1000);
+    }
+    else{
+        time_point<steady_clock> now = steady_clock::now();
+        float r = ((duration - duration_cast<milliseconds>(now-start)).count()/1000.f);
+        int remaining = r >= 0 ? (int)ceil(r) : (int)floor(r);
+        if (remaining < 0){
+            remaining = 0;
+        }
+        return remaining;
+    }
+}
--- Timer.h
+++ Timer.h
@@ -4,9 +4,25 @@
 
 #ifndef TIMERLAB_TIMER_H
 #define TIMERLAB_TIMER_H
+#include "chrono"
 
+using namespace std;
+using namespace std::chrono;
 
 class Timer {
+public:
+    Timer();
+
+        const time_point<steady_clock> &getStart() const;
+
+        int getDuration();
+        bool setDuration(unsigned int seconds);
+
+
+private:
+    bool running;
+    time_point<steady_clock> start;
+    ::duration<int, milli> duration;
 
 };
 
